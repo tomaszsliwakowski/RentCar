4918a10ae239f43031a7e077c51d5fd6b7263072
tomaszsliwakowski <tomek08082002@gmail.com>
2023-04-08 18:16:25 +0200

    Changes to be committed: 	modified:   client/package.json 	modified:   client/src/Routes/Routes.js 	modified:   client/src/components/header/header.jsx

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -10,7 +10,9 @@ export default function HeaderContainer() {
         <Header.LogoText to={ROUTES.Home}>YourCar</Header.LogoText>
       </Header.Group>
       <Header.Group>
-        <Header.Link to={ROUTES.Home}>Home</Header.Link>
+        <Header.Link exact to={ROUTES.Home}>
+          Home
+        </Header.Link>
         <Header.Link to={ROUTES.Cars}>Cars</Header.Link>
         <Header.LinkContact href="#footer">Contact Us</Header.LinkContact>
       </Header.Group>
